a736ff6004dd99fcfca64e6fa14c28542387f598
nryazanov13 <88179790+nryazanov13@users.noreply.github.com>
2025-08-06 18:45:36 +0300

    Update Main.java
    deleted comments

--- src/main/java/ru/nryazanov/Main.java
+++ src/main/java/ru/nryazanov/Main.java
@@ -1,10 +1,5 @@
 package ru.nryazanov;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
-
-
 public class Main {
 
     private static String msg = "Hello QA Guru!!!!";
