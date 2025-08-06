48183be9f72b34cf294f3ceb1334ccb47c30d761
nryazanov13 <88179790+nryazanov13@users.noreply.github.com>
2025-08-06 10:24:15 +0300

    Update Main.java
    added =

--- src/main/java/ru/nryazanov/Main.java
+++ src/main/java/ru/nryazanov/Main.java
@@ -3,10 +3,14 @@ package ru.nryazanov;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-private static String msg "Hello QA Guru!!!!";
+
 
 public class Main {
+
+    private static String msg = "Hello QA Guru!!!!";
+    
     public static void main(String[] args) {
         System.out.println(msg);
     }
+    
 }
